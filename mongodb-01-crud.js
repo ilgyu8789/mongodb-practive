379b7491dfc1a430696060b1938ce424822b5ebf
ilgyu8789 <85097158+ilgyu8789@users.noreply.github.com>
2021-09-07 11:49:01 +0900

    Find Query

--- mongodb-01-crud.js
+++ mongodb-01-crud.js
@@ -80,5 +80,70 @@ function testInsertMany(names) {
         testInsertOne(names);
     }
 }
-testInsertMany(["고길동", "둘리", "도우너"]);
+// testInsertMany(["고길동", "둘리", "도우너"]);
 // testInsertMany("징길산");
+
+//  삭제: 
+//  DELETE FROM friends [WHERE ...]
+//  db.friends.delete, db.friends.deleteMany({조건 객체})
+function testDeleteAll() {
+    client.connect()
+    .then(client => {
+        const db = client.db("mydb");
+        db.collection("friends").deleteMany({}) //  조건 객체
+        .then(result => {
+            console.log(result.deletedCount, "개 레코드 삭제");
+            client.close();
+        })
+    })
+}
+// testDeleteAll();
+
+function testInsertOneDoc(doc) {
+    client.connect()
+    .then(client => {
+        const db = client.db("mydb");
+        db.collection("friends")
+            .insertOne(doc)
+            .then(result => {
+                console.log(result.insertedId);
+                client.close();
+            })
+            .catch(reason => {
+                console.error(reason);
+            })
+    })
+}
+// testInsertOneDoc({name: "임꺽정", job: "도적"});
+
+function testInsertManyDocs(docs) {
+    client.connect()
+    .then(client => {
+        const db = client.db("mydb");
+        if (Array.isArray(docs)) {
+            //  여러 개의 문서
+            db.collection('friends').insertMany(docs)
+            .then(result => {
+                console.log(result.insertedCount, "개 삽입");
+                client.close();
+            })
+            .catch(reason => {
+                console.error(reason);
+            })
+        } else {
+            //  1개 문서
+            testInsertOneDoc(docs);
+        }
+    })
+}
+testInsertManyDocs(
+    [{name: '고길동', gender: '남성', species: '인간', age: 50},
+     {name: '둘리', gender: '남성', species: '공룡', age: 10000000},
+     {name: '도우너', gender: '남성', species: '외계인', age: 15},
+     {name: '또치', gender: '여성', species: '조류', age: 15},
+     {name: '영희', gender: '여성', species: '인간', age: 12}]);
+
+// 함수 내보내기: 다른 모듈에서 사용할 수 있게  
+exports.testInsertOneDoc = testInsertOneDoc;
+exports.testInsertManyDocs = testInsertManyDocs;
+exports.testDeleteAll = testDeleteAll;   
